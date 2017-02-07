d00e3fbf1bf69ccb938b10fd2c0ea3916cb44fb7
Bruno Marques <bruno@bmarques.net>
2017-02-07 17:33:36 -0200

    Replaced pathlib.iglob stuff

--- nbrunner.py
+++ nbrunner.py
@@ -1,3 +1,4 @@
+import glob
 import itertools
 import os
 import pathlib
@@ -17,12 +18,13 @@ def render():
     if not path.endswith('.ipynb'):
         abort(404)
     try:
-        fp = pathlib.Path(root, path).open('r')
+        filepath = pathlib.Path(root, path)
+        fp = filepath.open('r')
         notebook = nbformat.read(fp, as_version=4)
     except FileNotFoundError:
         abort(404)
     pre = ExecutePreprocessor()
-    pre.preprocess(notebook, {})
+    pre.preprocess(notebook, {'metadata': {'path': filepath.parent}})
     exporter = HTMLExporter()
     body, resources = exporter.from_notebook_node(notebook)
     return body
@@ -30,10 +32,15 @@ def render():
 
 @app.route('/')
 def index():
-    root = pathlib.Path(os.environ['NBRUNNER_ROOT'])
-    files = root.rglob('*.ipynb')
-    file_items = itertools.groupby(sorted(files), lambda f: f.parent)
-    return render_template('index.html', files=file_items, root=root)
+    # unfortunately, pathlib.{i,}glob cannot be used in most versions < 3.6
+    # due to a bug with permissions - see http://bugs.python.org/issue24120
+    root = os.path.normpath(os.environ['NBRUNNER_ROOT'])
+    files = glob.glob('{}/**/*.ipynb'.format(root), recursive=True)
+    file_items = itertools.groupby(sorted(files), lambda f: os.path.dirname(f))
+    folder_files = {}
+    for path, items in file_items:
+        folder_files[path] = [(os.path.relpath(file, root), os.path.basename(file)) for file in items]
+    return render_template('index.html', files=folder_files)
 
 
 if __name__ == '__main__':
