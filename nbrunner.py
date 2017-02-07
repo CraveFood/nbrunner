dd6b587e434443fa88080d24469a753ee86552b8
Bruno Marques <bruno@bmarques.net>
2017-02-07 11:04:26 -0200

    Notebook rendering

--- nbrunner.py
+++ nbrunner.py
@@ -2,14 +2,30 @@ import itertools
 import os
 import pathlib
 
-from flask import Flask, render_template
+import nbformat
+from flask import Flask, abort, render_template, request
+from nbconvert.exporters import HTMLExporter
+from nbconvert.preprocessors import ExecutePreprocessor
 
 app = Flask(__name__)
 
 
 @app.route('/render')
 def render():
-    pass
+    root = pathlib.Path(os.environ['NBRUNNER_ROOT'])
+    path = request.args.get('file', '')
+    if not path.endswith('.ipynb'):
+        abort(404)
+    try:
+        fp = pathlib.Path(root, path).open('r')
+        notebook = nbformat.read(fp, as_version=4)
+    except FileNotFoundError:
+        abort(404)
+    pre = ExecutePreprocessor()
+    pre.preprocess(notebook, {})
+    exporter = HTMLExporter()
+    body, resources = exporter.from_notebook_node(notebook)
+    return body
 
 
 @app.route('/')
